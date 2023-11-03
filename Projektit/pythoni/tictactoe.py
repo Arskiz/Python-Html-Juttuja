2d928715fe08bdff59157b36c97df46a04ccc364
Arskiz <129842259+Arskiz@users.noreply.github.com>
2023-11-03 22:16:09 +0200

    clean

--- Projektit/pythoni/tictactoe.py
+++ Projektit/pythoni/tictactoe.py
@@ -1,10 +1,10 @@
 import os
 import time
 
-
-
-
 tictactoe = [[0,0,0], [0,0,0], [0,0,0]]
+
+startingUser = "x"
+user = startingUser
 
 
 currentTicTacToe = ["",
@@ -14,7 +14,7 @@ currentTicTacToe = ["",
                     ""]
 
 def set_ticTacToe(target): # Line goes from top to bottom so 0 -> 2 
-    
+    global user
     lineRowType = []
     
     line = None
@@ -28,18 +28,17 @@ def set_ticTacToe(target): # Line goes from top to bottom so 0 -> 2
     row = int(lineRowType[1])
     type = lineRowType[2]
         
-        
-    print("Rivi:", str(line), "Kohta:", str(row), "Tyyppi:", type)
-    time.sleep(2)
-    
-    if(line in range(1,3) and row in range(1,3)):
-        if(type != "x" or type != "o"):
-            if line == 1:
-                    tictactoe[0][row -1] = charToMatrix(type)
-            if line == 2:
-                    tictactoe[1][row -1] = charToMatrix(type)
-            if line == 3:
-                    tictactoe[2][row -1] = charToMatrix(type)
+    if type == user and line > 0 < 4 and row > 0 < 4:
+        if line == 1:
+                tictactoe[0][row -1] = charToMatrix(type)
+        if line == 2:
+                tictactoe[1][row -1] = charToMatrix(type)
+        if line == 3:
+                tictactoe[2][row -1] = charToMatrix(type)
+        if(user == "x"):
+            user = "o"
+        elif(user == "o"):
+            user = "x"
     else:
         clearTerminal()
         print("|-----------------------------------------|")
@@ -69,20 +68,39 @@ def charToMatrix(char):
         return 2
 
 def handleVictory():
-    x = "x"
-    o = "o"
+    x = charToMatrix("x")
+    o = charToMatrix("o")
+    
     # horizontal check
-    for i in range(0,2):
+    for i in range(3):
         if(tictactoe[i][0] == x):
             if(tictactoe[i][1] == x):
                 if(tictactoe[i][2] == x):
-                    return("X")
-                
+                    return("x")
         elif(tictactoe[i][0] == o):
             if(tictactoe[i][1] == o):
-                if(tictactoe[i][2] == 0):
-                    return("O")
-                
+                if(tictactoe[i][2] == o):
+                    return("o")
+
+    # vertical check
+    for i in range(3):
+        if(tictactoe[0][i] == x):
+            if(tictactoe[1][i] == x):
+                if(tictactoe[2][i] == x):
+                    return("x")
+        elif(tictactoe[0][i] == o):
+            if(tictactoe[1][i] == o):
+                if(tictactoe[2][i] == o):
+                    return("o")
+
+    # diagonal check
+    if(tictactoe[0][0] == x and tictactoe[1][1] == x and tictactoe[2][2] == x) or \
+       (tictactoe[0][0] == o and tictactoe[1][1] == o and tictactoe[2][2] == o):
+        return("x" if tictactoe[0][0] == x else "o")
+
+    if(tictactoe[0][2] == x and tictactoe[1][1] == x and tictactoe[2][0] == x) or \
+       (tictactoe[0][2] == o and tictactoe[1][1] == o and tictactoe[2][0] == o):
+        return("x" if tictactoe[0][2] == x else "o")
     
             
 
@@ -94,6 +112,7 @@ def clearTerminal():
         
 
 def main():
+    global user
     clearTerminal()
     try:
         print("Hei! Tervetuloa ristinollaan!")
@@ -113,19 +132,23 @@ def main():
             
             time.sleep(0.1) # Save cpu usage   
                 
-
+            winner = handleVictory()
+            
+            if(winner == "x" or winner == "o"):
+                print("Winner is:", winner + "!!")
+                time.sleep(5)
+                break
+            
             print("-----------------------------")
             for i in currentTicTacToe:
                 print(i)
             print("-----------------------------")
-            winner = handleVictory()
-            if(winner == "X" or winner == "O"):
-                print("Winner is:", winner + "!!")
+            
 
             time.sleep(0.1)
             print("Sijainti kirjoitetaan tässä muodossa: ESIM: [11x] <--> ensimmäinen kirjain tarkoittaa että ylhäältä alhaalle päin katsottuna ylin rivi, toinen kirjain tarkoittaa horizontaalissa muodossa olevaa 3 eri kohtaa joita voi täyttää. viimeinen eli vaihtoehdot x tai o ovat tyyppi sille että kuka suorittaa siirron, x vai o")
             print("-----------------------------")
-            turn = input("Kirjoita sijainti: ")
+            turn = input(user + ", kirjoita sijainti: ")
             
             if(len(turn) != 3):
                 print("Opettele Paska! Liian vähän kirjoitit!")
